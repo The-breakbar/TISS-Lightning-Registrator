4044080ed9c13868b79c7249606e199dd807f1dd
The-breakbar <breakbar.email@gmail.com>
2023-02-19 01:20:09 +0100

    initial commit

---
+++ content-scripts/sendRegistration.js
@@ -0,0 +1,314 @@
+// This is a content script responsible for executing the registration
+// The main callback, which is called with a message, sends multiple requests to TISS until one of them succeeds
+// A single request consists of 2 POST request, the first one to get the confirmation page and the second one to confirm the registration
+
+// The following message format is required:
+// {
+// 	action: "sendRegistration"
+// 	timestamp: Timestamp of when the requests should start sending in milliseconds
+// 	startOffset: How many ms before the timestamp the refresh loop should start
+//  stopOffset: How many ms after the timestamp the refresh loop should stop
+// 	maxAttempts: Maximum number of attempts to send the register equest
+//  optionId: Id of the option, starting from the first "j_id" until the colon and number (e.g. "j_id_52:0:j_id_5d:j_id_5g:0") (only for group and exam)
+//  slot: A two string array containing the slot start and end time (e.g. ["10:00", "10:30"]) (only for exam)
+// }
+
+// The script will send a message with the following format when it is done:
+// {
+// 	action: "sendRegistrationResponse"
+// 	success: Boolean indicating if the registration was successful
+// 	attempts: Number of attempts it took to send the request
+// 	errors: Array of errors that occurred during the request loop
+// 	time: Time it took to send the requests in milliseconds
+// }
+
+// Documentation for the endpoints:
+
+// Every request has to have these 3 session cookies: _tiss_session, TISS_AUTH, JSESSIONID
+// The cookies generally do not change during the session, except for JSESSIONID, which can change if bad requests are sent
+
+// /education/course/courseRegistration.xhtml
+// This endpoint is used to get the confirmation page of lva registrations and requires the following POST data:
+// 	registrationForm:j_id_6t : "Register" ("Anmelden" is also valid) (the key of this parameter is the id of the register button)
+//  registrationForm_SUBMIT : "1"
+//  dspwid : A window id (found in the url as "dswid" or in the page)
+//  javax.faces.ClientWindow : Same value as dspwid
+//  javax.faces.ViewState : A valid ViewState (see below)
+
+// /education/course/groupList.xhtml
+// This endpoint is used to get the confirmation page of group registrations and requires the following POST data:
+//  groupContentForm:<id>:j_id_a1 : "Register" ("Anmelden" is also valid) (the id in the key is from the html ids of the option, see getPageInfo.js)
+//  groupContentForm_SUBMIT : "1"
+//  dspwid : A window id (found in the url as "dswid" or in the page)
+//  javax.faces.ClientWindow : Same value as dspwid
+//  javax.faces.ViewState : A valid ViewState obtained from the response to the previous request (see below)
+
+// /education/course/examDateList.xhtml
+// This endpoint is used to get the confirmation page of exam registrations and requires the following POST data:
+//  examDateListForm:<id>:j_id_9u : "Register" ("Anmelden" is also valid) (the id in the key is from the html ids of the option, see getPageInfo.js)
+//  examDateListForm_SUBMIT : "1"
+//  dspwid : A window id (found in the url as "dswid" or in the page)
+//  javax.faces.ClientWindow : Same value as dspwid
+//  javax.faces.ViewState : A valid ViewState obtained from the response to the previous request (see below)
+
+// /education/course/register.xhtml
+// This endpoint is used to confirm the registration, has to be called after one of the lva/group/exam endpoints was called
+// It requires the following POST data:
+//  regForm:j_id_30 : "Register" ("Anmelden" is also valid) (the key of this parameter is the id of the confirm button)
+//  regForm_Submit : "1"
+//  dspwid : A window id (found in the url as "dswid" or in the page)
+//  javax.faces.ClientWindow : Same value as dspwid
+//  javax.faces.ViewState : A valid ViewState obtained from the response to the previous request (see below)
+//  regForm:subgrouplist : Number value of the exam slot selection option (has to be extracted from the response to the previous request) (only for exams with slots)
+
+// ViewState (represents the state of the page)
+// The ViewState is a unique string that is required for every request and changes every time the page is loaded
+// A valid ViewState can only be used once for a request
+// It can be found in the page source as a hidden input field with the name "javax.faces.ViewState"
+// If a request wants to be sent to a registration that just opened, a new valid ViewState has to be obtained by refreshing the page
+// The response to the first registration request will contain a new ViewState, which can be used for the confirmation request
+
+// Set a cookie for future refresh (GET) requests, to prevent being redirected to the window handler page
+// To not get redirected to a blank window handler page, the request needs a cookie containing the dsrid value and the ClientWindow id
+// The dsrid value can be any 3-digit number, but the ClientWindow id has to be the same as the one in the url
+// (note that the cookie name is dsrwid, not dsrid as in the url)
+let windowId;
+if (document.location.href.includes("dswid")) windowId = document.location.href.match(/dswid=(\d*)/)[1]; // The window handler page may not have the id
+const DSRID_VALUE = 1;
+document.cookie = `dsrwid-${DSRID_VALUE}=${windowId}`;
+
+// This function "refreshes" the page with a GET request and returns a promise of the text of the response
+let getPage = async () => {
+	let response = await fetch(document.location.href.replace(/dsrid=\d*/, `dsrid=${DSRID_VALUE}`));
+	return response.text();
+};
+
+// Main callback
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	// Check if the message is a registration request
+	if (message.action != "sendRegistration") return;
+
+	// Log the time when the message was received
+	console.log(new Date().toLocaleTimeString() + "." + new Date().getMilliseconds() + " - Received registration request...");
+
+	// Setup the request loop
+	let { timestamp, startOffset, stopOffset, maxAttempts, optionId, slot } = message;
+	let attempts = 0;
+	let success = false;
+	let errors = [];
+	let viewState;
+
+	// Continously refresh the page until the register button is found, then get the ViewState and start the registration loop
+	// If the button is not found after the stopOffset specified time, the loop will stop
+	let remainingTime = Math.max(0, timestamp - Date.now() - startOffset);
+	setTimeout(async () => {
+		// Log the time when the loop started
+		console.log(new Date().toLocaleTimeString() + "." + new Date().getMilliseconds() + " - Starting refresh loop...");
+
+		let stopTime = Date.now() + startOffset + stopOffset;
+		while (Date.now() < stopTime) {
+			// Log that a refresh is being attempted
+			console.log("Refreshing page...");
+
+			// Refresh the page
+			let page = await getPage();
+			let pageDocument = new DOMParser().parseFromString(page, "text/html");
+			let options = pageDocument.querySelectorAll("#contentInner .groupWrapper");
+
+			// Find if the button is on the page
+			let button;
+			if (pageType == "lva") button = options[0].querySelector("#registrationForm\\:j_id_6t");
+			else button = Array.from(options).find((option) => option.querySelector(`input[id*="${optionId}"]`));
+
+			// If the button was found, extract the ViewState, stop the refresh loop and start the register loop
+			if (button) {
+				viewState = pageDocument.querySelector(`input[name="javax.faces.ViewState"]`).value;
+				registerLoop();
+				return;
+			}
+		}
+
+		// Log that the refresh loop timed out
+		console.log(new Date().toLocaleTimeString() + "." + new Date().getMilliseconds() + " - Refresh loop timed out, no button found...");
+
+		return;
+	}, remainingTime);
+
+	let registerLoop = async () => {
+		// Log the time when the starts being refreshed
+		console.log(new Date().toLocaleTimeString() + "." + new Date().getMilliseconds() + " - Valid ViewState obtained, starting register loop...");
+
+		let timeStart = Date.now();
+
+		// Request loop
+		while (attempts < maxAttempts && !success) {
+			attempts++;
+
+			// Attempt to send the requests
+			try {
+				// Get a new ViewState (except for the first attempt)
+				if (attempts > 1) {
+					// Log the time when the starts being refreshed
+					console.log(new Date().toLocaleTimeString() + "." + new Date().getMilliseconds() + " - Refreshing for new ViewState...");
+
+					let page = await getPage();
+					viewState = page.match(/<input type="hidden" name="javax.faces.ViewState" id=".*" value="(.*)" autocomplete="off" \/>/)[1];
+				}
+
+				// Returns true if the request was successful, otherwise throws an error
+				success = await sendRequest(viewState, optionId, slot);
+
+				// Log success
+				console.log(new Date().toLocaleTimeString() + "." + new Date().getMilliseconds() + " - Registration success with attempt number " + attempts);
+			} catch (error) {
+				// Log the error
+				console.log(new Date().toLocaleTimeString() + "." + new Date().getMilliseconds() + " - Attempt number " + attempts + " failed");
+				console.error(error);
+
+				// If the request fails, store the error for later
+				errors.push(error.message);
+			}
+		}
+
+		// Calculate the time it took to finish the loop
+		let time = Date.now() - timeStart;
+
+		// Log the time it took to send the requests
+		console.log(new Date().toLocaleTimeString() + "." + new Date().getMilliseconds() + ` - Registration loop finished (${time}ms)`);
+
+		// Get the count of already registered students
+		let page = await getPage();
+		let pageDocument = new DOMParser().parseFromString(page, "text/html");
+		let options = pageDocument.querySelectorAll("#contentInner .groupWrapper");
+		let numberString = Array.from(options)
+			.find((option) => pageType == "lva" || option.querySelector(`input[id*="${optionId}"]`))
+			.querySelector(`span[id*="members"]`).innerText;
+		let number = numberString.split("/")[0].trim();
+
+		// Debug
+		if (success) console.log("Registered as number " + number);
+
+		// Send the response to the popup
+		chrome.runtime.sendMessage({
+			action: "sendRegistrationResponse",
+			success,
+			attempts,
+			errors,
+			time,
+			number
+		});
+	};
+
+	// Close the message connection
+	sendResponse();
+});
+
+// Define the endpoints and POST data for the requests
+const LVA_ENDPOINT = "https://tiss.tuwien.ac.at/education/course/courseRegistration.xhtml";
+const GROUP_ENDPOINT = "https://tiss.tuwien.ac.at/education/course/groupList.xhtml";
+const EXAM_ENDPOINT = "https://tiss.tuwien.ac.at/education/course/examDateList.xhtml";
+const CONFIRM_ENDPOINT = "https://tiss.tuwien.ac.at/education/course/register.xhtml";
+
+const CONFIRM_DATA = {
+	"regForm:j_id_30": "Register",
+	regForm_SUBMIT: "1"
+};
+const LVA_DATA = {
+	"registrationForm:j_id_6t": "Register",
+	registrationForm_SUBMIT: "1"
+};
+// The group and exam data needs the id of the option to be inserted
+let getGroupData = (groupId) => {
+	let data = { groupContentForm_SUBMIT: "1" };
+	let idKey = `groupContentForm:${groupId}:j_id_a1`;
+	data[idKey] = "Register";
+	return data;
+};
+let getExamData = (examId) => {
+	let data = { examDateListForm_SUBMIT: "1" };
+	let idKey = `examDateListForm:${examId}:j_id_9u`;
+	data[idKey] = "Register";
+	return data;
+};
+
+// This function attempts to send the two POST requests required to register and returns true if successful
+// If any of the requests fail, it will throw an error (caused by the validateResponse function)
+let sendRequest = async (viewState, optionId, slot) => {
+	// Define the request body
+	let bodyData = {
+		dspwid: windowId,
+		"javax.faces.ClientWindow": windowId,
+		"javax.faces.ViewState": viewState
+	};
+
+	// Create the body together with the additional required data and define endpoint
+	let targetUrl, body;
+	if (pageType == "lva") {
+		targetUrl = LVA_ENDPOINT;
+		body = { ...bodyData, ...LVA_DATA };
+	} else if (pageType == "group") {
+		targetUrl = GROUP_ENDPOINT;
+		body = { ...bodyData, ...getGroupData(optionId) };
+	} else if (pageType == "exam") {
+		targetUrl = EXAM_ENDPOINT;
+		body = { ...bodyData, ...getExamData(optionId) };
+	}
+
+	// Define the request payload
+	let payload = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded"
+		},
+		body: new URLSearchParams(body).toString() // Encode the body as a query string
+	};
+
+	// Log the time when the first request is sent
+	console.log(new Date().toLocaleTimeString() + "." + new Date().getMilliseconds() + " - Sending register request with body: ", body);
+
+	// Send the first request
+	let firstResponse = await fetch(targetUrl, payload);
+	validateResponse(firstResponse);
+	let text = await firstResponse.text();
+
+	// Get the new ViewState from the response and add it to the payload
+	let newViewState = text.match(/<input type="hidden" name="javax.faces.ViewState" id=".*" value="(.*)" autocomplete="off" \/>/)[1];
+	bodyData["javax.faces.ViewState"] = newViewState;
+
+	// If the registration option has slots, get the slot id from the response and add it to the payload
+	if (slot) {
+		let optionArray = text.match(/<select id="regForm:subgrouplist".*<\/select>/)[0].split("<option");
+		let option = optionArray.find((option) => option.includes(slot[0]) && option.includes(slot[1]));
+		let value = option.match(/value="(.*)"/)[1];
+		bodyData["regForm:subgrouplist"] = value;
+	}
+
+	// Update the body with the new data and encode it
+	payload.body = new URLSearchParams({ ...bodyData, ...CONFIRM_DATA }).toString();
+
+	// Log the time when the second request is sent
+	console.log(new Date().toLocaleTimeString() + "." + new Date().getMilliseconds() + " - Sending confirm request with body: ", { ...bodyData, ...CONFIRM_DATA });
+
+	// Send the second request
+	let secondResponse = await fetch(CONFIRM_ENDPOINT, payload);
+	validateResponse(secondResponse);
+
+	// If both requests succeed, return true
+	return true;
+};
+
+// Function to validate the responses for either a redirect or a non-ok status
+let validateResponse = (response) => {
+	// Check if the response is a redirect, mostly redirects to a 404 or "Invalid session" page
+	// Caused by an invalid request (usually the ViewState is invalid)
+	if (response.redirected) {
+		let errorCode = response.url.match(/errorCode=(.*)/)[1];
+		throw new Error(`Response failed, redirected to ${errorCode} error page.`);
+	}
+
+	// Check if the response is not ok, generally happens if something is very wrong with the request
+	// (usually it is a 302, but a 500 has also been observed)
+	if (!response.ok) {
+		throw new Error(`Response failed with status ${response.status}.`);
+	}
+};
---
+++ index.js
@@ -0,0 +1,106 @@
+// If tab is on an appropriate TISS page, message getPageInfo.js content script in page to retrieve all the registration info
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+	// Check if tab has content script injected
+	const tabUrl = tabs[0].url;
+	if (!/https:\/\/.*tiss.tuwien.ac.at\/education\/course\/(courseRegistration|groupList|examDateList)/.test(tabUrl)) return;
+
+	// Determine type of registration
+	let registrationTask;
+	if (/courseRegistration/.test(tabUrl)) registrationTask = "lva";
+	else if (/groupList/.test(tabUrl)) registrationTask = "group";
+	else if (/examDate/.test(tabUrl)) registrationTask = "exam";
+	if (!registrationTask) return;
+
+	let startTime;
+	// Bind register callback
+	document.getElementById("register-button").addEventListener("click", () => {
+		startTime = Date.now();
+		let targetDateString, optionId, slot;
+		if (registrationTask == "lva") {
+			let optionInfo = pageInfo.options[0];
+			targetDateString = optionInfo.start;
+		} else {
+			optionId = document.getElementById("idselect").value;
+			let optionInfo = pageInfo.options.find((option) => option.id == optionId);
+			targetDateString = optionInfo.start;
+			if (optionInfo.slots) {
+				slot = document.getElementById("slotselect").value.split(",");
+			}
+		}
+		let [date, time] = targetDateString.split(", ");
+		let [day, month, year] = date.split(".");
+		let [hour, minute] = time.split(":");
+		let targetTime = new Date(year, month - 1, day, hour, minute).getTime();
+		let timeRemaining = Math.max(0, targetTime - Date.now());
+
+		let message = {
+			action: "sendRegistration",
+			timestamp: targetTime,
+			startOffset: 10000,
+			stopOffset: 20000,
+			maxAttempts: 7,
+			optionId,
+			slot
+		};
+		chrome.tabs.sendMessage(tabs[0].id, message);
+		document.getElementById("debug").textContent = `Registration started... (${Math.round(timeRemaining / 1000)}s remaining)`;
+	});
+
+	// Bind response callback
+	chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+		if (message.action != "sendRegistrationResponse") return;
+		let { success, attempts, errors, time, number } = message;
+		if (success) {
+			document.getElementById("debug").textContent = `Registration successful with ${attempts} attempt${attempts == 1 ? "" : "s"} (place ${number}) (${time}ms)`;
+		} else {
+			document.getElementById("debug").textContent = `Registration failed: Max attempts reached (${time}ms)`;
+		}
+
+		if (errors.length > 0) {
+			errors.forEach((errorMessage) => {
+				document.getElementById("debug").textContent += `\n${errorMessage}`;
+			});
+		}
+
+		sendResponse();
+	});
+
+	let pageInfo;
+	// Get page info on popup load
+	chrome.tabs.sendMessage(tabs[0].id, { action: "getPageInfo" }).then((response) => {
+		pageInfo = response;
+		document.getElementById("output").textContent = JSON.stringify(response, null, 2);
+
+		// Add select options
+		// Add options to select element
+		let select = document.getElementById("idselect");
+		pageInfo.options.forEach((option) => {
+			let opt = document.createElement("option");
+			opt.value = option.id;
+			opt.textContent = option.name;
+			select.appendChild(opt);
+		});
+
+		return;
+	});
+
+	// Bind slot selection callback
+	document.getElementById("idselect").addEventListener("change", (event) => {
+		let optionId = event.target.value;
+		let optionInfo = pageInfo.options.find((option) => option.id == optionId);
+		let slotSelect = document.getElementById("slotselect");
+		// Remove all options
+		while (slotSelect.firstChild) {
+			slotSelect.removeChild(slotSelect.firstChild);
+		}
+		// Add options to select element
+		if (optionInfo.slots) {
+			optionInfo.slots.forEach((slot) => {
+				let opt = document.createElement("option");
+				opt.value = slot.start + "," + slot.end;
+				opt.textContent = slot.start + " - " + slot.end;
+				slotSelect.appendChild(opt);
+			});
+		}
+	});
+});
