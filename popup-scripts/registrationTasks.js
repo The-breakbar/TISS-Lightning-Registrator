2fd86b802c2c42beaf5077069b4e7e75a4dd996d
The-breakbar <breakbar.email@gmail.com>
2023-02-23 22:53:44 +0100

    added timeouts for task removal after expiry

--- popup-scripts/registrationTasks.js
+++ popup-scripts/registrationTasks.js
@@ -14,20 +14,31 @@
 //  time : How long the registration took (if successful)
 // }
 
+let activeTimeouts = {};
+
+let initTaskRemovalTimeouts = async () => {
+	let taskList = Object.values(await chrome.storage.session.get(null));
+	let tasksToRemove = taskList.filter((task) => task.expiry < Date.now()).map((task) => task.tabId.toString());
+
+	// If there are any tasks that have expired, remove them
+	await chrome.storage.session.remove(tasksToRemove);
+
+	// Set up the timeouts for the remaining tasks
+	taskList.forEach((task) => {
+		activeTimeouts[task.tabId] = setTimeout(() => {
+			chrome.storage.session.remove(task.tabId.toString());
+		}, task.expiry - Date.now());
+	});
+
+	showTaskElements();
+};
+
 let showTaskElements = async () => {
 	let taskOutput = document.getElementById("tasks");
 	taskOutput.textContent = "";
 
 	let tasks = await chrome.storage.session.get(null);
 	let taskList = Object.values(tasks).sort((a, b) => a.created - b.created);
-	let tasksToRemove = taskList.filter((task) => task.expiry < Date.now()).map((task) => task.tabId.toString());
-
-	// If there are any tasks that have expired, remove them
-	// Return early because the removal will trigger this callback again
-	if (tasksToRemove.length > 0) {
-		chrome.storage.session.remove(tasksToRemove);
-		return;
-	}
 
 	taskList.forEach((task) => {
 		taskOutput.textContent += `[${task.status}] ${task.lva}\n${task.name} (${Math.round((task.target - Date.now()) / 1000)}s)\n`;
@@ -35,6 +46,28 @@ let showTaskElements = async () => {
 };
 
 // Bind update callback which will be triggered every time a task is added/updated/removed
+// Redraws the task elements and updates the removal timeouts
 chrome.storage.onChanged.addListener((changes, area) => {
 	if (area == "session") showTaskElements();
+
+	Object.keys(changes).forEach((key) => {
+		// If a task is removed, clear the timeout
+		if (changes[key].newValue == undefined) {
+			clearTimeout(activeTimeouts[key]);
+			delete activeTimeouts[key];
+		}
+		// If a task is added, set the timeout
+		else if (changes[key].oldValue == undefined) {
+			activeTimeouts[key] = setTimeout(() => {
+				chrome.storage.session.remove(key);
+			}, changes[key].newValue.expiry - Date.now());
+		}
+		// If the expiry of a task is updated, clear the old timeout and set a new one
+		else if (changes[key].newValue.expiry != changes[key].oldValue.expiry) {
+			clearTimeout(activeTimeouts[key]);
+			activeTimeouts[key] = setTimeout(() => {
+				chrome.storage.session.remove(key);
+			}, changes[key].newValue.expiry - Date.now());
+		}
+	});
 });
