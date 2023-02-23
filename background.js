6c1a853bf4a9cf15a3d141e23bd0c62e73b1b5cb
The-breakbar <breakbar.email@gmail.com>
2023-02-23 16:50:10 +0100

    removed debug console logs

--- background.js
+++ background.js
@@ -16,17 +16,13 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
 // Add a new registration task
 let addTask = ({ task }) => {
-	console.log("Adding task", task);
 	tasks.unshift(task);
-	console.log(tasks);
 };
 
 // Update an existing registration task
 let updateTask = ({ task }) => {
 	// Find the task with the same tabId
 	// Then take all the values from task and overwrite the existing task
-	console.log("Updating task", task);
 	let index = tasks.findIndex((t) => t.tabId == task.tabId);
 	tasks[index] = { ...tasks[index], ...task };
-	console.log(tasks);
 };
