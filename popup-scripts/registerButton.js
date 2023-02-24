dac59ec03448587691aca50d9514284423fac0fc
The-breakbar <breakbar.email@gmail.com>
2023-02-24 02:11:40 +0100

    removed unused code

--- popup-scripts/registerButton.js
+++ popup-scripts/registerButton.js
@@ -18,7 +18,6 @@ document.getElementById("register-button").addEventListener("click", async () =>
 	let [day, month, year] = date.split(".");
 	let [hour, minute] = time.split(":");
 	let targetTime = new Date(year, month - 1, day, hour, minute).getTime();
-	let timeRemaining = Math.max(0, targetTime - Date.now());
 
 	// Send the registration request to the content script
 	let message = {
@@ -44,23 +43,4 @@ document.getElementById("register-button").addEventListener("click", async () =>
 		expiry: Math.max(Date.now(), targetTime) + 30000
 	};
 	chrome.storage.session.set({ [tabId.toString()]: task });
-});
-
-// Bind response callback
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-	if (message.action != "sendRegistrationResponse") return;
-	let { success, attempts, errors, time, number } = message;
-	if (success) {
-		document.getElementById("output").textContent = `Registration successful with ${attempts} attempt${attempts == 1 ? "" : "s"} (place ${number}) (${time}ms)`;
-	} else {
-		document.getElementById("output").textContent = `Registration failed: Max attempts reached (${time}ms)`;
-	}
-
-	if (errors.length > 0) {
-		errors.forEach((errorMessage) => {
-			document.getElementById("output").textContent += `\n${errorMessage}`;
-		});
-	}
-
-	sendResponse();
 });
