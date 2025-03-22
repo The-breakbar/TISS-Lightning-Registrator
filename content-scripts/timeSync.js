ee042404a1864327e884be9feeec5da9601b1f5e
The-breakbar <breakbar.email@gmail.com>
2025-03-22 16:53:46 +0100

    changed timesync to always use api time

--- content-scripts/timeSync.js
+++ content-scripts/timeSync.js
@@ -1,5 +1,4 @@
 // This file handles getting the correct time from an API and checking if the local time is correct
-const TIME_DEVIATION_MAX = 30000; // 30 seconds
 const MAX_API_WAIT_TIME = 10000; // 10 seconds
 const controller = new AbortController();
 
@@ -8,7 +7,7 @@ const controller = new AbortController();
 // This is not a perfect solution, if the request fails, then the time is not checked
 // However this should only happen very rarely, as most system clocks don't deviate that much
 let getSyncedTime = async () => {
-	let timeOverride = Date.now(); // Default to local time
+	let currentTime = Date.now(); // Default to local time
 
 	setTimeout(() => controller.abort(), MAX_API_WAIT_TIME); // abort the request if it takes too long/is rate limited
 	const timeResponse = await fetch("https://timeapi.io/api/time/current/zone?timeZone=Europe%2FVienna", { signal: controller.signal }).catch((error) => {
@@ -21,12 +20,10 @@ let getSyncedTime = async () => {
 		const data = await timeResponse.json();
 		let serverTime = new Date(data.dateTime).getTime(); // Convert to timestamp
 		let timeDifference = Date.now() - serverTime;
-		console.log(`Local time: ${new Date(Date.now()).toLocaleString()}\nServer time: ${new Date(serverTime).toLocaleString()}\nDifference: ${timeDifference}ms`);
 
-		if (Math.abs(timeDifference) > TIME_DEVIATION_MAX) {
-			timeOverride = serverTime;
-		}
+		console.log(`Local time: ${new Date(Date.now()).toLocaleString()}\nServer time: ${new Date(serverTime).toLocaleString()}\nDifference: ${timeDifference}ms`);
+		currentTime = serverTime;
 	}
 
-	return timeOverride;
+	return currentTime;
 };
